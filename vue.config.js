79b008e8bb5ffeb813dece53dfb92b18444655ac
Иван <bushmin@list.ru>
2020-03-02 17:54:40 +0300

    removed dist_electron

--- vue.config.js
+++ vue.config.js
@@ -1,27 +1,27 @@
-const path = require('path');
+const path = require('path')
 module.exports = {
-    pages: {
-        index: {
-          entry: 'src/renderer/main.js',
-          template: 'public/index.html'
-        }
-      },
-      configureWebpack: {
-        resolve: {
-          alias: {
-            '@': path.resolve(__dirname, 'src/renderer'),
-            '@assets': path.resolve(__dirname, 'src/assets'),
-            '@icons': path.resolve(__dirname, 'src/renderer/components/icons'),
-            '@views': path.resolve(__dirname, 'src/renderer/views'),
-            '@components': path.resolve(__dirname, 'src/renderer/components'),
-            '@libs': path.resolve(__dirname, 'src/renderer/libs'),
-            '@classes': path.resolve(__dirname, 'src/renderer/classes')
-          }
-        }
-      },
-    pluginOptions: {
-      electronBuilder: {
-        mainProcessFile: 'src/main/index.js'
+  pages: {
+    index: {
+      entry: 'src/renderer/main.js',
+      template: 'public/index.html'
+    }
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src/renderer'),
+        '@assets': path.resolve(__dirname, 'src/assets'),
+        '@icons': path.resolve(__dirname, 'src/renderer/components/icons'),
+        '@views': path.resolve(__dirname, 'src/renderer/views'),
+        '@components': path.resolve(__dirname, 'src/renderer/components'),
+        '@libs': path.resolve(__dirname, 'src/renderer/libs'),
+        '@classes': path.resolve(__dirname, 'src/renderer/classes')
       }
     }
+  },
+  pluginOptions: {
+    electronBuilder: {
+      mainProcessFile: 'src/main/index.js'
+    }
   }
+}
