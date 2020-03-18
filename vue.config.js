5557a066f030e466fda51c5dec3e9e5a135605b7
ivan <bushmin@list.ru>
2020-03-18 19:22:57 +0300

    Tray manager, positioner class (with tray support)

--- vue.config.js
+++ vue.config.js
@@ -11,6 +11,7 @@ module.exports = {
   configureWebpack: {
     resolve: {
       alias: {
+        '@static': path.resolve(__dirname, 'static'),
         '@': path.resolve(__dirname, 'src/renderer'),
         '@assets': path.resolve(__dirname, 'src/assets'),
         '@icons': path.resolve(__dirname, 'src/renderer/components/icons'),
