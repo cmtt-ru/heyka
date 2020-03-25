da615dba6bd6506a0ef2bc1355ea081674c4bb3e
Michael Nalbandyan <xinger@ya.ru>
2020-03-25 21:53:40 +0300

    routes & layout

---
+++ src/renderer/views/MainWindow/Settings/index.vue
@@ -0,0 +1,50 @@
+<template>
+
+    <layout>
+
+      <template v-slot:sidebar-header>
+        Settings
+      </template>
+
+      <template v-slot:sidebar-body>
+        <router-link to="">General</router-link>
+        <br>
+        <router-link to="">Devices</router-link>
+        <br>
+        <router-link to="">Network</router-link>
+        <br>
+        <router-link to="">About</router-link>
+        <br>
+        <router-link to="">Support</router-link>
+      </template>
+
+      <template v-slot:content-body>
+        <router-link :to="{name: 'workspace'}">Close</router-link>
+        <router-view></router-view>
+      </template>
+
+    </layout>
+
+</template>
+
+<script>
+import Layout from './../Layout';
+
+export default {
+  components: {
+    Layout,
+  },
+  data() {
+    return {
+
+    };
+  },
+  created() {
+
+  },
+};
+</script>
+
+<style scoped lang="stylus">
+
+</style>
---
+++ src/renderer/views/MainWindow/Workspace/index.vue
@@ -0,0 +1,45 @@
+<template>
+
+    <layout>
+
+      <template v-slot:sidebar-header>
+        sidebar-header
+      </template>
+
+      <template v-slot:sidebar-body>
+        <router-link :to="{ name: 'settings'}">Settings</router-link>
+      </template>
+
+      <template v-slot:content-header>
+        content-header
+      </template>
+
+      <template v-slot:content-body>
+        <router-view></router-view>
+      </template>
+
+    </layout>
+
+</template>
+
+<script>
+import Layout from './../Layout';
+
+export default {
+  components: {
+    Layout,
+  },
+  data() {
+    return {
+
+    };
+  },
+  created() {
+
+  },
+};
+</script>
+
+<style scoped lang="stylus">
+
+</style>
