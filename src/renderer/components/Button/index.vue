8340f387014821bf01eba961f85cb84a45794843
Michael Nalbandyan <xinger@ya.ru>
2020-03-26 21:05:43 +0300

    initial

---
+++ src/renderer/components/Button/index.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="ui-button" :class="classes"></div>
+</template>
+
+<script>
+export default {
+  name: 'svg-icon',
+  props: {
+    /**
+     * Style type of the button
+     * @param [1 — 10]
+     */
+    type: {
+      type: Number,
+      default: 1,
+    },
+
+    /**
+     * Size of the button.
+     * @param small | medium | large
+     */
+    size: {
+      type: String,
+      default: 'medium',
+    },
+
+    /**
+     * Active state of the button
+     */
+    active: {
+      type: Boolean,
+      default: false,
+    },
+
+    /**
+     * Disabled state of the button
+     */
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+
+    /**
+     * Loading state of the button
+     */
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+
+    /**
+     * Make button to fill full width
+     */
+    wide: {
+      type: Boolean,
+      default: false,
+    },
+
+    /**
+     * Icon name
+     */
+    icon: {
+      type: String,
+      default: null,
+    },
+
+  },
+
+  computed: {
+
+  },
+
+  methods: {
+
+  },
+
+  mounted() {
+
+  },
+};
+</script>
+
+<style lang="stylus">
+
+</style>
