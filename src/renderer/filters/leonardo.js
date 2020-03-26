184e09c59861857e2802fdf1c3744e0720aa97d8
Michael Nalbandyan <xinger@ya.ru>
2020-03-26 19:15:11 +0300

    leonardo filter

---
+++ src/renderer/filters/leonardo.js
@@ -0,0 +1,48 @@
+import Vue from 'vue';
+
+/**
+ * Retina image size multiplicator
+ * @type {number}
+ */
+const retinaMultiplicator = 2;
+
+/**
+ * Checks if user has retina display
+ *
+ * @return {boolean}
+ */
+function isRetina() {
+  return window.devicePixelRatio > 1;
+}
+
+/**
+ * Apply image filter to leonardo url
+ *
+ * @param {string} src — leonardo image url
+ * @param {number} width – image width
+ * @param {number} height – image height
+ * @param {string} scaleType – scale type, default `scale_scrop`, can be 'resize'
+ * @return {string}
+ */
+function formImageUrl(src, width, height, scaleType = 'scale_crop') {
+  if (!src.includes('leonardo.osnova')) {
+    return '';
+  }
+
+  if (width === undefined) {
+    return '';
+  }
+
+  if (height === undefined) {
+    height = width;
+  }
+
+  if (isRetina()) {
+    width *= retinaMultiplicator;
+    height *= retinaMultiplicator;
+  }
+
+  return `${src}-/${scaleType}/${width}x${height}/center`;
+}
+
+Vue.filter('formImageUrl', formImageUrl);
