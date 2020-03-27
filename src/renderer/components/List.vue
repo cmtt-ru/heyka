28a2a996af8917bdc256a8b2c5e1d6c20ca4f0af
ivan <bushmin@list.ru>
2020-03-27 18:15:28 +0300

    Lists 1.0

---
+++ src/renderer/components/List.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="list-view">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+
+    };
+  },
+
+  props: {
+  },
+
+  computed: {
+
+  },
+
+};
+</script>
+
+<style lang="stylus">
+    .list-view
+        border 1px solid var(--text-0)
+</style>
