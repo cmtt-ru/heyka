e25ac275d20c9a6f9d5d39539c41a86984b401f7
ivan <bushmin@list.ru>
2020-03-27 13:40:51 +0300

    Color theme json structures, dynamic layout css vars

--- src/renderer/views/MainWindow/Layout.vue
+++ src/renderer/views/MainWindow/Layout.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="layout">
 
-      <div class="layout__column layout__column--sidebar">
+      <div :style="$themes.getColors('navbar')" class="layout__column layout__column--sidebar">
 
         <div class="layout__row layout__row--header" v-if="$slots['sidebar-header']">
           <slot name="sidebar-header"></slot>
@@ -14,7 +14,7 @@
 
       </div>
 
-      <div class="layout__column layout__column--content">
+      <div :style="$themes.getColors('content')" class="layout__column layout__column--content">
 
         <div class="layout__row layout__row--header" v-if="$slots['content-header']">
           <slot name="content-header"></slot>
@@ -43,10 +43,6 @@ export default {
 </script>
 
 <style lang="stylus">
-  /** TODO: move out colors */
-  $sidebar-bg = #EFEFF1;
-  $content-bg = #FFFFFF;
-  $border-color = alpha(#000, 0.075);
 
   .layout
     display flex
@@ -59,15 +55,17 @@ export default {
       &--sidebar
         width 200px
         flex 0 0 auto
-        background $sidebar-bg
+        background-color var(--app-bg)
+        color var(--text-0)
 
       &--content
         flex 3 0 auto
-        background $content-bg
+        background-color var(--app-bg)
+        color var(--text-0)
 
     &__row
       &--header
         height 39px
-        border-bottom 1px solid $border-color
+        border-bottom 1px solid var(--line-stroke)
 
 </style>
