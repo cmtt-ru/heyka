5be50ef772a16f9db20192fa40b540ddea9ef3f9
Michael Nalbandyan <xinger@ya.ru>
2020-03-31 19:20:48 +0300

    make styleguide

---
+++ src/renderer/views/MainWindow/Styleguide/index.vue
@@ -0,0 +1,72 @@
+<template>
+
+    <layout>
+
+      <template v-slot:sidebar-header>
+        <div class="l-p-8 l-w-m">
+          Styleguide
+        </div>
+      </template>
+
+      <template v-slot:content-header>
+        <div class="l-p-8 l-w-m">
+
+        </div>
+      </template>
+
+      <template v-slot:sidebar-body>
+        <div class="l-p-8">
+          <router-link class="link" :to="{name: 'styleguide-buttons'}">Buttons</router-link>
+          <router-link class="link" :to="{name: 'styleguide-lists'}">Lists</router-link>
+          <br>
+          <br>
+          <router-link class="link" :to="{name: 'workspace'}">Exit</router-link>
+        </div>
+      </template>
+
+      <template v-slot:content-body>
+        <div class="l-p-8">
+          <router-view></router-view>
+        </div>
+      </template>
+
+    </layout>
+
+</template>
+
+<script>
+import Layout from './../Layout';
+
+export default {
+  components: {
+    Layout,
+  },
+  data() {
+    return {
+
+    };
+  },
+  created() {
+
+  },
+};
+</script>
+
+<style scoped lang="stylus">
+  .link
+    display block
+    padding 4px 8px
+    border-radius 4px
+    font-size 14px
+    text-decoration none
+    color var(--text-0)
+    margin-bottom 2px
+
+    &:hover:not(.router-link-exact-active)
+      background-color var(--item-bg-hover)
+
+  .router-link-exact-active
+    background-color var(--item-bg-active)
+    box-shadow 0 1px 2px rgba(0, 0, 0, 0.1)
+
+</style>
