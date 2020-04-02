ca119caaa4baa3178ff9a962a023d0e13d6b1bd5
Michael Nalbandyan <xinger@ya.ru>
2020-04-02 21:53:10 +0300

    store structure

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createLogger from 'vuex/dist/logger';
+import modules from './modules';
+
+const debug = process.env.NODE_ENV !== 'production';
+
+console.log(modules);
 
 Vue.use(Vuex);
-
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  },
+const store = new Vuex.Store({
+  modules,
+  strict: debug,
+  plugins: debug ? [ createLogger() ] : [],
 });
+
+export default store;
