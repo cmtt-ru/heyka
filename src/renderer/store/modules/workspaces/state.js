925416f9462a7cd1ed88852476b73af0613b71c7
Michael Nalbandyan <xinger@ya.ru>
2020-04-03 19:28:15 +0300

    workspace module state

--- src/renderer/store/modules/workspaces/state.js
+++ src/renderer/store/modules/workspaces/state.js
@@ -1,6 +1,44 @@
 const state = () => {
   return {
-    workspace: Math.random(),
+    /**
+     * List of all available workspace
+     */
+    workspaces: {
+
+      0: {
+        /**
+         * Workspace id
+         * @type {number}
+         */
+        id: 0,
+
+        /**
+         * Workspace name
+         * @type {string}
+         */
+        name: 'test',
+
+        /**
+         * Workspace avatar
+         * Represents as leonardo url
+         * @type {string}
+         */
+        avatar: 'leonardo-image-src',
+
+        /**
+         * Workspace creation date
+         * @type {string}
+         */
+        createdAt: '',
+
+        /**
+         * Workspace update date
+         * @type {string}
+         */
+        updatedAt: '',
+      },
+
+    },
   };
 };
 
