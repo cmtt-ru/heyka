f50a41fbb8ba774188261082f3186fe1d3b55f1b
Michael Nalbandyan <xinger@ya.ru>
2020-04-03 20:15:32 +0300

    users module state

--- src/renderer/store/modules/users/state.js
+++ src/renderer/store/modules/users/state.js
@@ -1,6 +1,63 @@
 const state = () => {
   return {
-    workspace: Math.random(),
+    /**
+     * List of all available users in specific workspace
+     */
+    collection: {
+
+      0: {
+        /**
+         * User id
+         * @type {string}
+         */
+        id: '',
+
+        /**
+         * User name
+         * @type {string}
+         */
+        name: 'test',
+
+        /**
+         * User email
+         * @type {string}
+         */
+        email: 'email@test.io',
+
+        /**
+         * Email verification status
+         * @type {boolean}
+         */
+        isEmailVerified: true,
+
+        /**
+         * User avatar
+         * Represents as leonardo url
+         * @type {string}
+         */
+        avatar: 'leonardo-image-src',
+
+        /**
+         * User creation date
+         * @type {string}
+         */
+        createdAt: '',
+
+        /**
+         * User update date
+         * @type {string}
+         */
+        updatedAt: '',
+
+        /**
+         * Online status
+         * @type {string}
+         */
+        onlineStatus: 'online',
+
+      },
+
+    },
   };
 };
 
