6314b7b32c1e2df615d736e244ffae6caf349105
Michael Nalbandyan <xinger@ya.ru>
2020-04-03 18:50:42 +0300

    app module state

--- src/renderer/store/modules/app/state.js
+++ src/renderer/store/modules/app/state.js
@@ -1,6 +1,62 @@
 const state = () => {
   return {
-    some: Math.random(),
+    /**
+     * App name
+     * @used on about page
+     * @type {string}
+     */
+    appName: 'Heyka Desktop',
+
+    /**
+     * App version
+     * @used on about page
+     * @type {string}
+     */
+    appVersion: '1.1.4',
+
+    /**
+     * App language
+     * @type {string}
+     */
+    language: 'en',
+
+    /**
+     * Run app from tray or dock
+     * `tray` | `dock`
+     * @type {string}
+     */
+    runAppFrom: 'tray',
+
+    /**
+     * App autorun
+     * @type {boolean}
+     */
+    autorun: true,
+
+    /**
+     * App color theme
+     * @type {{auto: boolean, name: string}}
+     */
+    theme: {
+      name: 'Dark',
+      auto: true,
+    },
+
+    /**
+     * List of user device
+     * @type {{speakers: array, microphones: array, webcams: array}}
+     */
+    devices: {
+      speakers: [],
+      microphones: [],
+      webcams: [],
+    },
+
+    /**
+     * Whether to collect analytics
+     * @type {boolean}
+     */
+    collectAnalytics: true,
   };
 };
 
