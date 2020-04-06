b854c905bac5cd2e101201d1d8e51af55773f230
ivan <bushmin@list.ru>
2020-04-06 18:03:43 +0300

    custom v-fade directive

--- src/renderer/directives/index.js
+++ src/renderer/directives/index.js
@@ -1,24 +1,4 @@
 import Vue from 'vue';
+import textfade from './textFade';
 
-/**
- * Decide on el resize if we should add custom fade text-overflow
- */
-const fadeObserver = new ResizeObserver(entries => {
-  for (const entry of entries) {
-    if (entry.target.offsetWidth < entry.target.scrollWidth) {
-      entry.target.classList.add('text-overflow');
-    } else {
-      entry.target.classList.remove('text-overflow');
-    }
-  }
-});
-
-Vue.directive('textfade', {
-  inserted: (el) => {
-    fadeObserver.observe(el);
-  },
-  unbind: (el) => {
-    fadeObserver.unobserve(el);
-  },
-
-});
+Vue.directive('textfade', textfade);
