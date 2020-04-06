046865059ace68a18c9d84edaec4cf5b522d579e
Michael Nalbandyan <xinger@ya.ru>
2020-04-07 00:08:44 +0300

    some docs

--- src/renderer/store/modules/me/getters.js
+++ src/renderer/store/modules/me/getters.js
@@ -1,4 +1,10 @@
 export default {
+  /**
+   * Get selected workspace id
+   *
+   * @param {object} state – me module state
+   * @return {string}
+   */
   selectedWorkspaceId: state => {
     return state.selectedWorkspaceId;
   },
