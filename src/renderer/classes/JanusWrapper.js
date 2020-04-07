fbec7178dc20891ecb8a8244ec6bbbce908b6f95
Evgenii K <aksyline@gmail.com>
2020-04-08 00:50:53 +0300

    added audiobridge plugin class

--- src/renderer/classes/JanusWrapper.js
+++ src/renderer/classes/JanusWrapper.js
@@ -1,5 +1,6 @@
 import { EventEmitter } from 'events';
 import Janus from './janus';
+import AudiobridgePlugin from './AudiobridgePlugin';
 
 const ERROR_CODES = {
   SERVER_DOWN: 'Server is down',
@@ -15,23 +16,45 @@ class JanusWrapper extends EventEmitter {
   /**
    * Creates an instance for Janus connection
    * @param {object} config Janus connection parameters
-   * @param {string} config.janusServerUrl Url of Janus server
-   * @param {string} config.janusAuthToken Authenticate token for Janus connection
+   * @param {string} config.url Url of Janus server
+   * @param {string} config.workspaceToken Authentication token for Janus connection
+   * @param {string} config.channelToken Authentication token for channels
+   * @param {number} config.audioRoomId Janus audio room id
+   * @param {number} config.videoRoomId Janus video room id
+   * @param {string} config.userId Application user id
    * @param {boolean} config.debug Enable or disable debug output
    */
-  constructor({ janusServerUrl, janusAuthToken, debug }) {
+  constructor({
+    url,
+    workspaceToken,
+    channelToken,
+    audioRoomId,
+    videoRoomId,
+    userId,
+    debug,
+  }) {
     super();
 
     // Initialize private variables
-    this.__janusServerUrl = janusServerUrl;
-    this.__janusAuthToken = janusAuthToken;
-    this.__janus = null;
+    this.__url = url;
+    this.__workspaceToken = workspaceToken;
+    this.__channelToken = channelToken;
+    this.__audioRoomId = audioRoomId;
+    this.__videoRoomId = videoRoomId;
+    this.__userId = userId;
     this.__debug = debug;
+
+    this.__janus = null;
+
+    // plugins
+    this.__audiobridgePlugin = null;
+    this.__videoroomPlugin = null;
   }
 
   /**
    * Initialized janus library
    * It is needed to be called just after app started
+   * @static
    * @return {Promise<null>}
    */
   static init() {
@@ -47,16 +70,48 @@ class JanusWrapper extends EventEmitter {
   }
 
   /**
+   * Attaches media stream to an HTML5 audio element
+   * @static
+   * @param {object} element HTML element
+   * @param {object} stream Media stream
+   * @return {undefined}
+   */
+  static attachMediaStream(element, stream) {
+    Janus.attachMediaStream(element, stream);
+  }
+
+  /**
+   * Connects to the server and join to channels
+   * @public
+   * @returns {Promise<Stream>} Audio stream from Janus server
+   */
+  async join() {
+    /** Connect to Janus */
+    await this.__connect();
+
+    const audiobridgePlugin = new AudiobridgePlugin({
+      janus: this.__janus,
+      room: this.__audioRoomId,
+      token: this.__channelToken,
+      userId: this.__userId,
+      debug: true,
+    });
+
+    audiobridgePlugin.attach();
+  }
+
+  /**
    * Connects to the Janus server
+   * @private
    * @returns {Promise<null>}
    */
-  connect() {
+  __connect() {
     return new Promise((resolve, reject) => {
       let isFullfilled = false;
 
       this.__janus = new Janus({
-        server: this.__janusServerUrl,
-        token: this.__janusAuthToken,
+        server: this.__url,
+        token: this.__workspaceToken,
         success: () => {
           resolve();
           isFullfilled = true;
@@ -72,7 +127,7 @@ class JanusWrapper extends EventEmitter {
             internalError = ERROR_CODES.UNKNOW;
           }
 
-          this._debug('Janus connection error', cause);
+          this.__debug('Janus connection error', cause);
           if (isFullfilled) {
             this.emit('connection-error', internalError);
 
@@ -90,9 +145,10 @@ class JanusWrapper extends EventEmitter {
 
   /**
    * Disconnect with server and close all active traffic channels
+   * @private
    * @returns {undefined}
    */
-  disconnect() {
+  __disconnect() {
     if (!this.__janus) {
       return;
     }
@@ -104,9 +160,10 @@ class JanusWrapper extends EventEmitter {
 
   /**
    * Inner debug tool
+   * @private
    * @returns {undefined}
    */
-  _debug() {
+  __debug() {
     if (this.__debug) {
       console.log('JANUS WRAPPER: ', ...arguments);
     }
