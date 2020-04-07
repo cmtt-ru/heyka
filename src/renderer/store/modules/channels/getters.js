0a3f330446ff274f4f8eb8cd3674f480763b869b
Michael Nalbandyan <xinger@ya.ru>
2020-04-07 13:46:18 +0300

    js docs

--- src/renderer/store/modules/channels/getters.js
+++ src/renderer/store/modules/channels/getters.js
@@ -2,6 +2,12 @@ import { sortAny } from '@libs/arrays';
 
 export default {
 
+  /**
+   * Ger sorted channels
+   *
+   * @param {object} state – channels module state
+   * @return {array}
+   */
   getChannels: state => {
     return Object.values(state.collection).sort(sortAny([
       {
