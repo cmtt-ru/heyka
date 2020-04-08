0c1b972c2989dc317426a7af21e9900bbe0b776e
ivan <bushmin@list.ru>
2020-04-08 13:17:59 +0300

    HEYK-79: active channel

--- src/renderer/views/MainWindow/Workspace/Channel.vue
+++ src/renderer/views/MainWindow/Workspace/Channel.vue
@@ -1,10 +1,41 @@
 <template>
   <div class="l-p-8">
 
-      <div>Канал {{ $route.params.id }}</div>
-      <br>
+      <div v-if="channel" class="channel-info">
+        <svg-icon v-if="!channel.talking" class="channel-info__type" :name="dynamicIcon" size="medium" stroke="var(--icon-1)"></svg-icon>
+        <svg-icon v-if="channel.talking" class="channel-info__type" name="channelOnAir" size="medium" animate>
+          <channel-on-air></channel-on-air>
+        </svg-icon>
+        <div v-textfade="channel.name" class="channel-info__name">{{channel.name}}</div>
+        <ui-button
+          :type="7"
+          class="channel-info__more"
+          size="small"
+          height="16"
+          @click.native="moreHandler()"
+          icon="more">
+        </ui-button>
 
- <list :filterBy="''">
+        <ui-button
+          v-if="!isConnected"
+          :type="1"
+          class="channel-info__connect"
+          size="small"
+          @click.native="clickConnectHandler()">
+          Join
+        </ui-button>
+        <ui-button
+          v-if="isConnected"
+          :type="4"
+          class="channel-info__connect"
+          size="small"
+          @click.native="clickDisconnectHandler()">
+          Disconnect
+        </ui-button>
+
+      </div>
+
+    <list :filterBy="''">
       <list-item
         @click.native="clickUserHandler(user)"
         v-for="user in users"
@@ -23,12 +54,23 @@
 <script>
 import { List, ListItem } from '@components/List';
 import UserItem from '@components/UserItem';
+import UiButton from '@components/UiButton';
+import channelOnAir from '@assets/iconsAnimate/channelOnAir.vue';
+
+const ICON_MAP = {
+  public: 'channel',
+  private: 'lock',
+  temp: 'clock',
+  default: 'channel',
+};
 
 export default {
   components: {
     List,
     ListItem,
     UserItem,
+    UiButton,
+    channelOnAir, // TODO: добавить и остальные анимированные иконки
   },
 
   computed: {
@@ -38,12 +80,63 @@ export default {
      * @returns {Array} array of users
      */
     users() {
-      return this.$store.getters.getUsersByChannel(this.$route.params.id);
+      return this.$store.getters.getUsersByChannel(this.channelId);
+    },
+
+    /**
+     * Get channel ID from route param
+     * @returns {String} – channel ID
+     */
+    channelId() {
+      return this.$route.params.id;
+    },
+
+    /**
+     * Returns channel
+     * @returns {object} – channel
+     */
+    channel() {
+      return this.$store.getters['channels/getChannelById'](this.channelId);
+    },
+
+    isConnected() {
+      return this.$store.getters['me/getSelectedChannelId'] === this.channelId;
+    },
+
+    /**
+     * Show icon corresponding to channel status
+     * @returns {String} name of correct icon
+     */
+    dynamicIcon() {
+      if (this.channel.isPrivate) { // TODO: lifespan
+        return ICON_MAP['private'];
+      } else {
+        return ICON_MAP['public'];
+      }
     },
 
   },
 
   methods: {
+
+    /**
+     * Connect to channel
+     * @returns {void}
+     */
+    async clickConnectHandler() {
+      // TODO: добавить коннект к сокетам и всё такое
+      await this.$store.dispatch('selectChannel', this.channelId);
+    },
+
+    /**
+     * Disconnect from channel
+     * @returns {void}
+     */
+    async clickDisconnectHandler() {
+      // TODO: добавить дисконнект от сокетов и всё такое
+      await this.$store.dispatch('unselectChannel', this.channelId);
+    },
+
     /**
      * Dummy click handler
      * @param {Object} user user
@@ -51,6 +144,14 @@ export default {
      */
     clickUserHandler(user) {
       console.log(user.name);
+    },
+
+    /**
+     * Dummy popover creation
+     * @returns {void}
+     */
+    moreHandler() {
+      console.log('more');
     },
   },
 
@@ -62,5 +163,32 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+
+.channel-info
+  height 32px
+  padding 0 4px 8px 0
+  margin-bottom 8px
+  width 100%
+  box-sizing border-box
+  display flex
+  flex-direction row
+  align-items center
+  justify-content flex-start
+
+  &__type
+    margin 0 4px
+    flex-shrink 0
+
+  &__name
+    font-weight 500
+    flex-grow 1
+
+  &__more
+    color var(--icon-1)
+    margin 0 8px
+    flex-shrink 0
+
+  &__connect
+    flex-shrink 0
 
 </style>
