ea2bd138c598fcbceb3dd91a2b8b7685a99e0730
ivan <bushmin@list.ru>
2020-04-08 14:35:17 +0300

    jsdocs and small fixes

--- src/renderer/store/modules/channels/actions.js
+++ src/renderer/store/modules/channels/actions.js
@@ -1,17 +1,5 @@
-import API from '@api';
+// import API from '@api';
 
 export default {
-
-  async selectChannel({ commit }, id, mediaState) {
-    const channel = await API.channel.select(id, mediaState/* {
-      microphone: false,
-      speakers: false,
-      screen: true,
-      camera: true,
-      speaking: false,
-    } */);
-
-    return channel;
-  },
 
 };
