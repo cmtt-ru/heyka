f3a8cbf44763a904b874913ce11a53e3b61c5d31
Michael Nalbandyan <xinger@ya.ru>
2020-04-08 03:00:37 +0300

    select channel done

--- src/renderer/store/modules/me/getters.js
+++ src/renderer/store/modules/me/getters.js
@@ -9,7 +9,23 @@ export default {
     return state.selectedWorkspaceId;
   },
 
+  /**
+   * Get media state
+   *
+   * @param {object} state – me module state
+   * @return {object}
+   */
   getMediaState: state => {
     return state.mediaState || {};
+  },
+
+  /**
+   * Get selected channel id
+   *
+   * @param {object} state – me module state
+   * @return {string}
+   */
+  getSelectedChannelId: state => {
+    return state.selectedChannelId;
   },
 };
