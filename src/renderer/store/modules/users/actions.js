0e82320afa2aeb013b559925e477fa32aaac80de
Michael Nalbandyan <xinger@ya.ru>
2020-04-10 20:06:50 +0300

    user online status event

--- src/renderer/store/modules/users/actions.js
+++ src/renderer/store/modules/users/actions.js
@@ -1,10 +1,17 @@
+import API from '@api';
+
 export default {
 
-  action({ commit }) {
-    return new Promise((resolve, reject) => {
-      commit('SET_SOME', 1);
-      resolve();
-    });
+  /**
+   * Set user online status
+   *
+   * @param {function} commit – vuex commit
+   * @param {string} status – online status
+   * @return {Promise<void>}
+   */
+  async setOnlineStatus({ commit }, status) {
+    commit('SET_ONLINE_STATUS', status);
+    await API.user.setOnlineStatus(status);
   },
 
 };
