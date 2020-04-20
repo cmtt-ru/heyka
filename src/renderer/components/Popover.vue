557f93e2b33e95462de8a1e84e09d813ebd9e320
Michael Nalbandyan <xinger@ya.ru>
2020-04-20 11:23:25 +0300

    new button type --11

--- src/renderer/components/Popover.vue
+++ src/renderer/components/Popover.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>Popover</div>
+  <div class="popover">
+    <slot/>
+  </div>
 </template>
 
 <script>
@@ -21,3 +23,15 @@ export default {
   },
 };
 </script>
+
+<style lang="stylus">
+  .popover
+    width 130px
+    text-align center
+    background #fff
+    padding 10px
+    border 0.5px solid rgba(0, 0, 0, 0.2)
+    box-shadow 0 3px 8px rgba(0, 0, 0, 0.15), 0 0 8px rgba(0, 0, 0, 0.15)
+    border-radius 4px
+    z-index 100
+</style>
