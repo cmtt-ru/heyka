e5cf517eeec86098dac5312413686fd77cf6ca41
Michael Nalbandyan <xinger@ya.ru>
2020-04-20 11:49:40 +0300

    popver styles

--- src/renderer/views/ContextMenus/Some.vue
+++ src/renderer/views/ContextMenus/Some.vue
@@ -1,15 +1,20 @@
 <template>
   <popover>
-    Some
+    <ui-button :type="11" icon="add">Invite members</ui-button>
+    <ui-button :type="11" icon="edit">Manage workspace</ui-button>
+    <ui-button :type="11" icon="settings">Settings</ui-button>
+    <ui-button :type="11" icon="lock">Quit Heyka</ui-button>
   </popover>
 </template>
 
 <script>
 import Popover from '@components/Popover';
+import UiButton from '@components/UiButton';
 
 export default {
   components: {
     Popover,
+    UiButton,
   },
 
   props: {
