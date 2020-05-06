5fc60667f43228c7fea7cab86da5ae58de6bd5ff
ivan <bushmin@list.ru>
2020-05-06 13:15:31 +0300

    scrollbar done right

--- src/renderer/views/MainWindow/Settings/Network.vue
+++ src/renderer/views/MainWindow/Settings/Network.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="settings-page">
     <div class="privacy-text">{{texts.transparency}}</div>
-    <textarea readonly cols="30" rows="10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae dignissimos nam illo quasi rem modi sed architecto voluptates aperiam quas? Ea aperiam in nihil, assumenda atque a similique eaque delectus! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus pariatur beatae dolore reiciendis eveniet laudantium incidunt eaque quos laborum nobis. Saepe hic reiciendis minima at, iure vero molestiae molestias provident! Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nam aut corrupti distinctio hic rem a ipsum rerum officia eaque! Sunt commodi sint obcaecati a distinctio earum ratione vel ipsam?</textarea>
+    <textarea class="scroll scroll-vh" readonly cols="30" rows="10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae dignissimos nam illo quasi rem modi sed architecto voluptates aperiam quas? Ea aperiam in nihil, assumenda atque a similique eaque delectus! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus pariatur beatae dolore reiciendis eveniet laudantium incidunt eaque quos laborum nobis. Saepe hic reiciendis minima at, iure vero molestiae molestias provident! Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nam aut corrupti distinctio hic rem a ipsum rerum officia eaque! Sunt commodi sint obcaecati a distinctio earum ratione vel ipsam?</textarea>
     <div class="settings__label">{{texts.googleLabel}}</div>
     <ui-switch @input="save('Analytics', analytics)" v-model="analytics" :text="texts.googleSwitch"/>
   </div>
