c18ee8bf8dc3684af7ae2fbde976f8d08dfc1b7b
Michael Nalbandyan <xinger@ya.ru>
2020-05-13 16:46:59 +0300

    move sharingSourceId from me to janus

--- src/renderer/store/modules/janus/mutations.js
+++ src/renderer/store/modules/janus/mutations.js
@@ -11,4 +11,15 @@ export default {
     Object.assign(state, options);
   },
 
+  /**
+   * Set's sharing source id
+   *
+   * @param {JanusState} state – module me state
+   * @param {string} id – source id
+   * @constructor
+   */
+  SET_SHARING_SOURCE_ID(state, id) {
+    state.sharingSourceId = id;
+  },
+
 };
