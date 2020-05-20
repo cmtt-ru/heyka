bd5ff0261fd20d8e3275175598ea96a5fe2ed6e0
Evgenii K <aksyline@gmail.com>
2020-05-20 23:05:39 +0300

    fix

--- src/renderer/classes/VideoroomPlugin.js
+++ src/renderer/classes/VideoroomPlugin.js
@@ -229,7 +229,7 @@ class VideoroomPlugin extends EventEmitter {
   requestVideoStream(janusId) {
     this.__janus.attach({
       plugin: JANUS_PLUGIN,
-      opaqueId: this.__userId,
+      opaqueId: this.__userId+'s',
       success: pluginHandle => {
         this._debug(`Subscription plugin attached`);
         pluginHandle.createOffer = function () {
