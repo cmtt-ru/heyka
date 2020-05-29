eb3e87232c1a57861a29a32cc4223d1d4f8a348d
Evgenii Korolev <aksyline@gmail.com>
2020-05-29 13:54:22 +0300

    HEYK-142: Janus videosharing (#60)
    * add videoroom plugin
    
    * Subscribe for events before socket auth
    
    * add videoroom plugin
    
    * Start sharing video from camera and screen
    
    * fix publish video stream on janus
    
    * fix2
    
    * send unpublish event to the janus
    
    * small fix
    
    * experimental version
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * undo changes in CallWindow.vue
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * refactor video sharing code
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * final (i hope) fix
    
    * literally not final fixes
    
    * may be these are final fixes
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fixes
    
    * stop sharing local video
    
    * video bug
    
    * overlay crop video
    
    * grid video cover
    
    * overflow: hidden;
    
    * hide video when user is not sharing
    
    * fix
    
    * fix
    
    * fix
    
    * okay
    
    * added host-receier
    
    * fix
    
    * fix
    
    * fix final-2-final-final-3
    
    * fix after review
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * add janus progress status
    
    * new janus
    
    * Revert "add janus progress status"
    
    This reverts commit dc74c9ed179da042319add7ec09e6ab3fe53c644.
    
    * Revert "Revert "add janus progress status""
    
    This reverts commit cd12e3c2bfb71a65325dcfc1c3f8cd529667913a.
    
    * return to old janus
    
    * REQUEST_VIDEOSTREAM_TIMEOUT
    
    * timeout before janus request stream
    
    * some console logs and fixes
    
    * fps
    
    * no fps
    
    * fix
    
    * stop request stream
    
    * f
    
    * debug
    
    * new janus.js
    
    * janus fixes
    
    * fix
    
    * fix stupid renaming
    
    * Update Janus.vue
    
    * fix
    
    * fix
    
    * preapre for merging
    
    * prepare for merging 2
    
    * force websockets
    
    * detach all subscriber plugins
    
    Co-authored-by: Michael Nalbandyan <xinger@ya.ru>
    Co-authored-by: ivan <bushmin@list.ru>

---
+++ src/renderer/classes/StreamSharing/Receiver.js
@@ -0,0 +1,152 @@
+import { EventEmitter } from 'events';
+import broadcastEvents from '../broadcastEvents';
+// import uuid from 'uuid/v4';
+
+/**
+ * StreamSharingReceiver`
+ * Manages sharing media streams on receiver side
+ * @class
+ */
+export default class StreamSharingReceiver extends EventEmitter {
+  /**
+   * Init stream sharing manager
+   * @param {object} options Stream sharing receiver manager options
+   * @param {boolean} [options.debug=false] Is debug enabled
+   */
+  constructor(options) {
+    super();
+
+    this.__debugEnabled = !!options.debug;
+    this.__pcs = {};
+    broadcastEvents.on('stream-sharing-closed', this._streamSharingClosed.bind(this));
+    broadcastEvents.on('clear-all', this._onClearAll.bind(this));
+  }
+
+  /**
+   * Requests stream of given user from main window
+   * @param {string} userId Which user you want to request stream
+   * @returns {void}
+   */
+  requestStream(userId) {
+    const data = {
+      // eslint-disable-next-line
+      requestId: `${Math.floor(Math.random() * 9999 + 10000)}`,
+      userId,
+    };
+    const pc = new RTCPeerConnection();
+
+    const onIceCandidate = e => {
+      if (!e.candidate) {
+        return;
+      };
+      this._debug(`ice-candidate ${data.requestId}`, e.candidate.toJSON());
+
+      // send ICE candidate to another window
+      broadcastEvents.dispatch(`icecandidate-receiver-${data.requestId}`, {
+        type: 'candidate',
+        candidate: e.candidate.toJSON(),
+      });
+    };
+
+    const onIceConnectionStateChange = e => {
+      this._debug(`ice-state ${data.requestId}`, pc.iceConnectionState);
+      if (pc && (pc.iceConnectionState === 'failed' || pc.iceConnectionState === 'disconnected')) {
+        this._streamSharingClosed(userId, data.requestId);
+      }
+    };
+
+    const onTrack = track => {
+      console.log(`%c Got track for ${userId}! `, 'background: yellow;');
+      this._debug(`Cought track from RTCPeerConnection ${data.requestId}: `, track);
+      // notify about new received stream
+      this.emit(`new-stream-${userId}`, {
+        userId,
+        stream: track.streams[0],
+      });
+    };
+
+    this.__pcs[userId] = {
+      requestId: data.requestId,
+      pc,
+      onTrack,
+      onIceConnectionStateChange,
+      onIceCandidate,
+    };
+
+    // subscribe for ICE candidate
+    pc.addEventListener('icecandidate', onIceCandidate);
+    // Handle ICE connection state change
+    pc.addEventListener('iceconnectionstatechange', onIceConnectionStateChange);
+    // Handle new track of the connection
+    pc.addEventListener('track', onTrack);
+
+    // handle offer from main window
+    broadcastEvents.once(`stream-offer-host-${data.requestId}`, async (d) => {
+      this._debug(`offer ${data.requestId}`, d);
+      await pc.setRemoteDescription(d.sdpOffer);
+      const answer = await pc.createAnswer();
+
+      await pc.setLocalDescription(answer);
+      // send answer
+      broadcastEvents.dispatch(`sdp-answer-receiver-${data.requestId}`, {
+        sdpAnswer: {
+          sdp: answer.sdp,
+          type: 'answer',
+        },
+      });
+    });
+
+    // handle ICE candidates from stream host
+    broadcastEvents.on(`icecandidate-host-${data.requestId}`, async (evt) => {
+      this._debug(`add ice candidate ${data.requestId}`, evt);
+      await pc.addIceCandidate(evt.candidate);
+    });
+
+    // Request stream
+    broadcastEvents.dispatch('request-stream', data);
+  }
+
+  /**
+   * Closes connection for userId
+   * @param {string} userId Close connection for userId
+   * @param {?string} requestId Request id
+   * @returns {void}
+   */
+  _streamSharingClosed(userId, requestId) {
+    const pc = this.__pcs[userId];
+
+    console.log(requestId, pc, requestId !== pc.requestId);
+    if (requestId && pc && requestId !== pc.requestId) {
+      return;
+    }
+    console.log(`%c Close connection for ${userId} (${requestId})! `, 'background: yellow;');
+
+    this._debug(`close connection for ${pc.requestId}, ${userId}`, pc);
+
+    pc.pc.close();
+    pc.pc.removeEventListener('icecandidate', pc.onIceCandidate);
+    pc.pc.removeEventListener('iceconnectionstatechange', pc.onIceConnectionStateChange);
+    pc.pc.removeEventListener('track', pc.onTrack);
+    delete this.__pcs[userId];
+
+    this.emit('connection-closed', userId);
+  }
+
+  /**
+   * On clear all peer connections
+   * @returns {void}
+   */
+  _onClearAll() {
+    Object.keys(this.__pcs).forEach(userId => this._streamSharingClosed(userId));
+  }
+
+  /**
+   * Inner debug tool
+   * @returns {void}
+   */
+  _debug() {
+    if (this.__debugEnabled) {
+      console.log('Stream sharing receiver: ', ...arguments);
+    }
+  }
+}
--- src/renderer/store/getters.js
+++ src/renderer/store/getters.js
@@ -56,6 +56,30 @@ export default {
   },
 
   /**
+   * Get user who share's screen or camera
+   *
+   * @param {object} state – global state
+   * @param {object} getters – global getters
+   * @returns {null|string}
+   */
+  getUsersWhoShareMedia: (state, getters) => {
+    const selectedChannelId = getters['me/getSelectedChannelId'];
+    const selectedChannel = getters['channels/getChannelById'](selectedChannelId);
+
+    if (selectedChannel) {
+      const usersWhoSharesScreen = selectedChannel.users.filter(user => user.screen).map(user => user.userId);
+      const usersWhoSharesCamera = selectedChannel.users.filter(user => user.camera).map(user => user.userId);
+
+      return [
+        ...usersWhoSharesCamera,
+        ...usersWhoSharesScreen,
+      ];
+    }
+
+    return [];
+  },
+
+  /**
    * Check's that I'am sharing screen or camera
    *
    * @param {object} state – global state
