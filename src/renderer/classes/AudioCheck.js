b88e74baae7bc42daaee562333b005902607de47
Иван <bushmin@list.ru>
2020-06-01 18:53:23 +0300

    all audio tests to AudioCheck

--- src/renderer/classes/AudioCheck.js
+++ src/renderer/classes/AudioCheck.js
@@ -1,5 +1,12 @@
+import { EventEmitter } from 'events';
 import store from '@/store';
 import hark from 'hark';
+import shutdown from 'electron-shutdown-command';
+import router from '@/router';
+import i18n from '@/i18n';
+
+const texts = i18n.t('notifications');
+
 const { systemPreferences } = require('electron').remote;
 
 /**
@@ -11,14 +18,23 @@ const audioTest = new Audio(require('@assets/audio/test-sound.mp3'));
 /**
  * Class for detecting AudioTrouble
  */
-class AudioCheck {
+class AudioCheck extends EventEmitter {
   /**
  * Init checker
  */
   constructor() {
+    super();
     this._changeDevice();
-    this.mediaStream = null;
-    this.harkInstance = null;
+    this.__mediaStream = null;
+    this.__harkInstance = null;
+  }
+
+  /**
+     * List of all devices
+     * @returns {object}
+     */
+  _devices() {
+    return store.getters['app/getDevices'];
   }
 
   /**
@@ -44,7 +60,7 @@ class AudioCheck {
   async _changeDevice() {
     this.destroyMediaStream();
 
-    this.mediaStream = await navigator.mediaDevices.getUserMedia({
+    this.__mediaStream = await navigator.mediaDevices.getUserMedia({
       audio: {
         deviceId: this._selectedMicrophone,
       },
@@ -52,14 +68,13 @@ class AudioCheck {
 
     audioTest.setSinkId(this._selectedDevices().speaker);
 
-    this.harkInstance = hark(this.mediaStream, {
-      interval: 1000,
-      threshold: -100,
+    this.__harkInstance = hark(this.__mediaStream, {
+      interval: 100,
     });
 
-    this.harkInstance.on('volume_change', (db) => {
+    this.__harkInstance.on('volume_change', (db) => {
       this.microphoneVolume = db;
-      // console.log(db);
+      this.emit('volume_change', db);
     });
   }
 
@@ -68,13 +83,21 @@ class AudioCheck {
    * @returns {void}
    */
   destroyMediaStream() {
-    if (this.harkInstance) {
-      this.harkInstance.stop();
+    if (this.__harkInstance) {
+      this.__harkInstance.stop();
     }
 
-    if (this.mediaStream) {
-      this.mediaStream.getTracks().forEach(track => track.stop());
+    if (this.__mediaStream) {
+      this.__mediaStream.getTracks().forEach(track => track.stop());
     }
+  }
+
+  /**
+     * Play test sound
+     * @returns {void}
+     */
+  playTestSound() {
+    audioTest.play();
   }
 
   /**
@@ -82,67 +105,97 @@ class AudioCheck {
  * @returns {void}
  */
   async checkAudio() {
-    const checkDelay = 1100; // milliseconds
+    if (this._checkNoMic()) {
+      return true;
+    };
+
+    const checkDelay = 100; // milliseconds
+    const sufficientAmount = 20; // times
     const vol1 = this.microphoneVolume;
 
-    await new Promise(resolve => setTimeout(resolve, checkDelay));
-    const vol2 = this.microphoneVolume;
-
-    if (vol1 === vol2) {
-      await this.__checkNoPermission();
-      await this.__checkNoSound();
+    for (let i = 0; i < sufficientAmount; i++) {
+      await new Promise(resolve => setTimeout(resolve, checkDelay));
+      if (this.microphoneVolume !== vol1) {
+        return true;
+      }
     }
+
+    if (this._checkNoPermission()) {
+      return true;
+    };
+    this._checkNoSound();
   }
 
   /**
    * Check if user doesn't have any mic available
    * @returns {void}
    */
-  __checkNoMic() {
+  async _checkNoMic() {
+    if (this._devices().microphones.length === 0) {
+      const notification = {
+        data: {
+          text: texts.nomic.text,
+          buttons: [
+            {
+              text: texts.nomic.button1,
+              type: 1,
+              close: true,
+            },
+          ],
+        },
+      };
 
+      await store.dispatch('app/addNotification', notification);
+
+      return true;
+    } else {
+      return false;
+    };
   }
 
   /**
    * Check if user has somehow no sound detected (but has connected mic)
    * @returns {void}
    */
-  async __checkNoSound() {
-    const notification = {
-      data: {
-        text: `No audio detected. Maybe restart system?`,
-        buttons: [
-          {
-            text: 'Restart',
-            type: 12,
-            //! add restart functionality?
-          },
-          {
-            text: 'Discard',
-            type: 1,
-            close: true,
-          },
-        ],
-      },
-    };
-
-    await store.dispatch('app/addNotification', notification);
-  }
-
-  /**
-   * Check if user didn't give any permission to use his mic
-   * @returns {void}
-   */
-  async __checkNoPermission() {
-    if (process.platform !== 'darwin') {
-      return false;
-    }
-    if (systemPreferences.getMediaAccessStatus('microphone') !== 'granted') {
+  async _checkNoSound() {
+    console.log(this._devices().microphones);
+    if (this._devices().microphones.length === 2) {
       const notification = {
         data: {
-          text: `You didn't give us access to mic. Go to Apple menu > System Preferences > Security & Privacy > Privacy > Microphone`,
+          text: texts.othermic.text,
           buttons: [
             {
-              text: 'OK',
+              text: texts.othermic.button1,
+              type: 1,
+              action: () => {
+                router.push({ name: 'settings-devices' });
+              },
+            },
+            {
+              text: texts.othermic.button2,
+              close: true,
+            },
+          ],
+        },
+      };
+
+      await store.dispatch('app/addNotification', notification);
+    } else {
+      const notification = {
+        data: {
+          text: texts.noaudio.text,
+          buttons: [
+            {
+              text: texts.noaudio.button1,
+              type: 12,
+              action: () => {
+                //! asks password on mac!
+                shutdown.reboot({ sudo: true });
+              },
+            },
+            {
+              text: texts.noaudio.button2,
+              type: 1,
               close: true,
             },
           ],
@@ -151,6 +204,42 @@ class AudioCheck {
 
       await store.dispatch('app/addNotification', notification);
     }
+  }
+
+  /**
+   * Check if user didn't give any permission to use his mic
+   * @returns {void}
+   */
+  async _checkNoPermission() {
+    if (process.platform !== 'darwin') {
+      return false;
+    }
+    if (systemPreferences.getMediaAccessStatus('microphone') !== 'granted') {
+      const notification = {
+        data: {
+          text: texts.nomicpermission.text,
+          buttons: [
+            {
+              text: texts.nomicpermission.button1,
+              type: 1,
+              action: () => {
+                window.open('x-apple.systempreferences:', '_blank');
+              },
+            },
+            {
+              text: texts.nomicpermission.button2,
+              close: true,
+            },
+          ],
+        },
+      };
+
+      await store.dispatch('app/addNotification', notification);
+
+      return true;
+    } else {
+      return false;
+    };
   }
 }
 
