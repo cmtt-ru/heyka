d2fdeca7920b053362486074401435f3538276f8
Evgenii K <aksyline@gmail.com>
2020-06-09 23:12:01 +0300

    fix

--- src/renderer/store/modules/app/state.js
+++ src/renderer/store/modules/app/state.js
@@ -5,38 +5,38 @@ const heykaStore = new Store({
   name: 'app',
 });
 
-//
-// @typedef {object} AppState
-// @property {string} appName – app name
-// @property {string} appVersion – app version
-// @property {string} language – language
-// @property {string} runAppFrom – run from tray or dock
-// @property {boolean} autorun – autorun
-// @property {boolean} collectAnalytics – whether to collect analytics
-// @property {array} privacyLogs – privacy logs
-//
-// @property {object} theme – color theme
-// @property {string} theme.name – theme name
-// @property {boolean} theme.auto – theme auto choose
-//
-// @property {object} devices – list of user devices
-// @property {boolean} devices.speakers – speakers
-// @property {boolean} devices.microphones – microphones
-// @property {boolean} devices.cameras – web cameras
-//
-// @property {object} selectedDevices – list of selected devices
-// @property {string} selectedDevices.speaker – selected speaker
-// @property {string} selectedDevices.microphone – selected microphone
-// @property {string} selectedDevices.camera – selected camera
-//
-// @property {number} microphoneVolume – current microphone volume in decibels
-// @property {array} notifications – in-app notifications
-// @property {string} search – workspace search
-//
-// @property {object} socket – current socket parameter
-// @property {string} socket.id – socket id
-// @property {number} socket.connectedAt – last time when socket was connected
-//
+/**
+ * @typedef {object} AppState
+ * @property {string} appName – app name
+ * @property {string} appVersion – app version
+ * @property {string} language – language
+ * @property {string} runAppFrom – run from tray or dock
+ * @property {boolean} autorun – autorun
+ * @property {boolean} collectAnalytics – whether to collect analytics
+ * @property {array} privacyLogs – privacy logs
+ *
+ * @property {object} theme – color theme
+ * @property {string} theme.name – theme name
+ * @property {boolean} theme.auto – theme auto choose
+ *
+ * @property {object} devices – list of user devices
+ * @property {boolean} devices.speakers – speakers
+ * @property {boolean} devices.microphones – microphones
+ * @property {boolean} devices.cameras – web cameras
+ *
+ * @property {object} selectedDevices – list of selected devices
+ * @property {string} selectedDevices.speaker – selected speaker
+ * @property {string} selectedDevices.microphone – selected microphone
+ * @property {string} selectedDevices.camera – selected camera
+ *
+ * @property {object} socket – current socket parameters
+ * @property {string} socket.id – id
+ * @property {number} socket.connectedAt – last time when socket was connected
+ *
+ * @property {number} microphoneVolume – current microphone volume in decibels
+ * @property {array} notifications – in-app notifications
+ * @property {string} search – workspace search
+ */
 
 /**
  * App state
