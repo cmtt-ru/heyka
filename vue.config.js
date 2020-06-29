46f9910e8e4e9621ba5cfa85d0afe57a1f72977a
ivan <bushmin@list.ru>
2020-06-29 16:12:22 +0300

    init

--- vue.config.js
+++ vue.config.js
@@ -42,7 +42,7 @@ module.exports = {
       },
     },
     plugins: webpackPlugins,
-    devtool: !IS_DEV ? 'hidden-source-map' : false,
+    devtool: IS_DEV ? 'cheap-source-map' : 'hidden-source-map',
   },
 
   pluginOptions: {
