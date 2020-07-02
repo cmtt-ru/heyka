ebe2e94f5661d524465b7b83e02d03a826d4e5a2
Иван <bushmin@list.ru>
2020-07-02 18:54:07 +0300

    changelogs, removed performance monitor from prod (#182)

--- src/renderer/views/MainWindow/Settings/About.vue
+++ src/renderer/views/MainWindow/Settings/About.vue
@@ -15,24 +15,32 @@
     </div>
 
     <a class="about-link">{{ texts.notice }}</a>
+    <div class="changelog">
+      <div
+        v-for="change in CHANGELOG"
+        :key="change.version"
+      >
+        <div class="changelog__version">
+          v{{ change.version }}
+        </div>
+
+        <div class="changelog__text">
+          {{ change.text }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 
-// import UiButton from '@components/UiButton';
-// import { UiSwitch, UiSelect, UiInput } from '@components/Form';
+import { CHANGELOG } from '@/changelog';
 
 export default {
-  components: {
-    // UiSwitch,
-    // UiSelect,
-    // UiInput,
-    // UiButton,
-  },
 
   data() {
     return {
+      CHANGELOG,
       info: this.$store.getters['app/getGeneralInfo'],
     };
   },
@@ -73,4 +81,23 @@ export default {
 .about-link
   padding-top 12px
   color var(--color-2)
+
+.changelog
+  display flex
+  text-align left
+  width 100%
+  padding 20px 0
+  box-sizing border-box
+  align-items flex-start
+  justify-content center
+  font-size 12px
+
+  &__version
+    font-size 2em
+    padding-bottom 15px
+
+  &__text
+    color var(--text-0)
+    white-space pre-line
+    line-height 20px
 </style>
