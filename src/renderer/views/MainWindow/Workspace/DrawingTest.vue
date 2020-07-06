0b46e949590dadf7acd324788b291bba10514d20
ivan <bushmin@list.ru>
2020-07-06 19:42:22 +0300

    send-recieve packets logic

--- src/renderer/views/MainWindow/Workspace/DrawingTest.vue
+++ src/renderer/views/MainWindow/Workspace/DrawingTest.vue
@@ -1,24 +1,40 @@
 <template>
-  <canvas
-    id="myCanvas"
-    class="drawing"
-    @mousemove="throttleSavePosition"
-  />
+  <div class="canvas-holder">
+    <canvas
+      id="myCanvas"
+      class="drawing"
+      @mousemove="throttleSavePosition"
+    />
+    <div
+      class="dot"
+      :style="cursorCoords"
+    />
+  </div>
 </template>
 
 <script>
 import { throttle } from 'throttle-debounce';
 const DELAY = 20;
+const SEND_DELAY = 250;
 
 export default {
   data() {
     return {
       ctx: {},
-      lastDot: null,
+
+      sendDots: [],
+      lastDrawDot: null,
+      recieveDots: [],
+      recieveDrawInterval: null,
     };
   },
   computed: {
-
+    cursorCoords() {
+      return {
+        top: `${this.lastDrawDot?.y}px`,
+        left: `${this.lastDrawDot?.x}px`,
+      };
+    },
   },
   mounted() {
     const canvas = document.getElementById('myCanvas');
@@ -32,28 +48,60 @@ export default {
     this.ctx = canvas.getContext('2d');
   },
   methods: {
-    throttleSavePosition:
-      throttle(DELAY, false, function ($event) {
-        const dot = {
-          x: $event.offsetX,
-          y: $event.offsetY,
-        };
+    throttleSavePosition: throttle(DELAY, false, function ($event) {
+      const dot = {
+        x: $event.offsetX,
+        y: $event.offsetY,
+        time: Math.round($event.timeStamp),
+      };
 
-        if (this.lastDot) {
-          this.ctx.moveTo(this.lastDot.x, this.lastDot.y);
-          this.ctx.lineTo(dot.x, dot.y);
-          this.ctx.stroke();
-        }
-        this.lastDot = dot;
-      }),
-    // dotPosition(dot) {
-    //   // console.log(dot);
+      this.sendDots.push(dot);
+      this.throttleSendDots();
+    }),
 
-    //   return {
-    //     top: `${dot.y}px`,
-    //     left: `${dot.x}px`,
-    //   };
-    // },
+    throttleSendDots: throttle(SEND_DELAY, false, function () {
+      this.addDots(this.sendDots);
+    }),
+
+    addDots(incomeDots) {
+      const newIndex = incomeDots.findIndex(el => el.time === this.recieveDots[this.recieveDots.length - 1]?.time);
+
+      if (newIndex === -1) {
+        this.recieveDots = [ ...incomeDots ];
+      } else {
+        this.recieveDots = [...this.recieveDots, ...[ ...incomeDots ].splice(newIndex)];
+      }
+
+      setTimeout(() => {
+        this.parseRecievedDots();
+      }, SEND_DELAY);
+    },
+
+    parseRecievedDots() {
+      if (this.recieveDots.length === 1 || this.recieveDrawInterval !== null) {
+        return;
+      }
+      this.recieveDrawInterval = setInterval(() => {
+        this.draw();
+      }, DELAY);
+    },
+
+    draw() {
+      if (this.recieveDots.length === 1) {
+        clearInterval(this.recieveDrawInterval);
+        this.recieveDrawInterval = null;
+
+        return;
+      }
+      const dot = { ...this.recieveDots.shift() };
+
+      if (this.lastDrawDot) {
+        this.ctx.moveTo(this.lastDrawDot.x, this.lastDrawDot.y);
+        this.ctx.lineTo(dot.x, dot.y);
+        this.ctx.stroke();
+      };
+      this.lastDrawDot = { ...dot };
+    },
   },
 };
 </script>
@@ -66,7 +114,8 @@ export default {
     border 1px solid var(--color-1)
     box-sizing border-box
     position relative
-
+.canvas-holder
+    position relative
 .dot
     position absolute
     width 5px
