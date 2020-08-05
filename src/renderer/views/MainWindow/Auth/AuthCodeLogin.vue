06e33036f63d64f5755bbba9f94c3fe15f59cf98
Michael Nalbandyan <xinger@ya.ru>
2020-08-05 18:16:21 +0300

    fix deep link

---
+++ src/renderer/views/MainWindow/Auth/AuthCodeLogin.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    logining...
+  </div>
+</template>
+
+<script>
+
+export default {
+  mounted() {
+    this.login(this.$route.query.hash);
+  },
+  methods: {
+    async login(code) {
+      await this.$API.auth.signinByLink(code);
+
+      await this.$store.dispatch('initial');
+
+      await this.$router.replace({
+        name: 'workspace',
+      });
+    },
+  },
+};
+</script>
