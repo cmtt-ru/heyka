8b41fd8b3ac1e239926dfc65c172b567443f307f
ivan <bushmin@list.ru>
2020-08-20 17:59:38 +0300

    v-sticky done

--- src/renderer/directives/sticky.js
+++ src/renderer/directives/sticky.js
@@ -1,4 +1,8 @@
-const STICKED_CLASS = 'ui-sticky';
+import { v4 as uuid4 } from 'uuid';
+
+const STICKED_CLASS = 'ui-sticked';
+
+const stickyObservers = {};
 
 /**
  * Decide if we need to update styles
@@ -7,24 +11,36 @@ const STICKED_CLASS = 'ui-sticky';
  */
 function elementCheck(el) {
   console.log(el);
-  if (el.intersectionRatio === 0) {
+  if (el.intersectionRatio === 1) {
     el.target.classList.add(STICKED_CLASS);
   } else {
     el.target.classList.remove(STICKED_CLASS);
   }
 }
 
-const stickyObserver = new IntersectionObserver(entries => {
-  for (const entry of entries) {
-    elementCheck(entry);
-  }
-});
-
 export default {
   inserted: (el) => {
-    stickyObserver.observe(el);
+    const id = uuid4();
+
+    stickyObservers[id] = new IntersectionObserver(entries => {
+      for (const entry of entries) {
+        elementCheck(entry);
+      }
+    }, {
+      root: el.parentElement,
+      threshold: 1,
+    });
+    stickyObservers[id].observe(el);
+    el.setAttribute('sticky-instance-uid', id);
+    el.style.position = 'sticky';
+    el.style.top = '0';
+    el.style.transform = 'translateY(-0.1px)';
   },
+
   unbind: (el) => {
-    stickyObserver.unobserve(el);
+    const id = el.getAttribute('sticky-instance-uid');
+
+    stickyObservers[id].unobserve(el);
+    delete stickyObservers[id];
   },
 };
