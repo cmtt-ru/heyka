5aef7383401d9526590062ec1ec5da88dda8b575
ivan <bushmin@list.ru>
2020-09-04 18:36:11 +0300

    hawk & bitrate stuff

--- src/renderer/classes/SpeedTest.js
+++ src/renderer/classes/SpeedTest.js
@@ -1,14 +1,20 @@
 /* eslint-disable require-jsdoc */
 import { EventEmitter } from 'events';
 import JanusEvents from './janusEvents';
-import API from '@api';
+// import API from '@api';
 
-const TEST_DATA_SIZE = 20000000;
-const MAX_API_TIME = 2000;
-const CHECK_AMOUNT = 3;
+// const TEST_DATA_SIZE = 20000000;
+// const MAX_API_TIME = 2000;
+// const CHECK_AMOUNT = 3;
 // const CRITICAL_DELTA = 0.4;
-const MAX_TIME = 10000;
+// const MAX_TIME = 10000;
 // const MIN_TIME = 1000;
+
+const TEST_TIME = 6;
+
+const MEDIUM_BITRATE = 25;
+const BAD_BITRATE = 20;
+const AWFUL_BITRATE = 10;
 
 /**
  * Class for testing network speed.
@@ -16,68 +22,101 @@ const MAX_TIME = 10000;
 class SpeedTest extends EventEmitter {
   constructor() {
     super();
-    this.uploadBitrate = 1000000;
-    this.checkUpload();
-    this.uploadSpeed = null;
-    this.uploadTimeout = null;
+
+    this.bitrates = [];
 
     JanusEvents.on('joined', () => {
-      console.log('joined, from speedtest');
+      // console.log('joined, from speedtest');
     });
     JanusEvents.on('bitrate', (bitrate) => {
-      console.log(bitrate.outvalue);
+      this.addBitrate({ ...bitrate });
+    });
+    JanusEvents.on('left', () => {
+      this.bitrates = [];
     });
   }
 
-  async checkUpload() {
-    const speed = await this.checkSpeed('upload');
-
-    console.log('Upload: ', speed, 'MB/s');
-    this.uploadSpeed = speed;
-    this.uploadTimeout = setTimeout(() => {
-      this.checkUpload();
-    }, MAX_TIME);
+  addBitrate(val) {
+    this.bitrates = this.bitrates.slice(-TEST_TIME);
+    this.bitrates.push(val);
+    this.testBitrateDrop();
   }
 
-  async checkDownload() {
-    const speed = await this.checkSpeed('download');
-
-    console.log('Download: ', speed, 'MB/s');
-  }
-
-  async checkSpeed(type, amount = CHECK_AMOUNT) {
-    const times = [];
-
-    for (let i = 0; i < amount; i++) {
-      const t0 = performance.now();
-
-      if (type === 'upload') {
-        let formData = new FormData();
-
-        formData.append('data', this.generateRandomData());
-        await API.app.checkUpload(formData);
-        formData = null;
-      } else {
-        await API.app.checkDownload();
-      }
-      const t1 = performance.now();
-
-      if (t1 - t0 > MAX_API_TIME) {
-        return TEST_DATA_SIZE / (t1 - t0);
-      }
-
-      times.push(Math.floor(t1 - t0));
+  testBitrateDrop() {
+    if (!window.hawk || this.bitrates.length < TEST_TIME) {
+      return;
     }
-    // console.log(times);
-    const time = times.reduce((a, b) => a + b, 0) / amount;
-    const speed = TEST_DATA_SIZE / time;
 
-    return speed;
+    const meanbitrate = this.simpleMean(this.bitrates, 'outvalueInt');
+
+    // console.log('MEAN BITRATE:', meanbitrate);
+
+    if (meanbitrate <= AWFUL_BITRATE) {
+      window.hawk.send(new Error('AWFUL BITRATE (<10)'), this.bitrates);
+    } else if (meanbitrate <= BAD_BITRATE) {
+      window.hawk.send(new Error('BAD BITRATE (<20)'), this.bitrates);
+    } else if (meanbitrate <= MEDIUM_BITRATE) {
+      window.hawk.send(new Error('MEDIUM BITRATE (<25)'), this.bitrates);
+    }
   }
 
-  generateRandomData() {
-    return new Blob([ new ArrayBuffer(TEST_DATA_SIZE) ], { type: 'multipart/form-data' });
-  };
+  simpleMean(values, key) {
+    values = values.filter(el => el[key]).map(el => el[key]);
+
+    const sum = values.reduce((a, b) => a + b, 0);
+    const mean = sum / values.length;
+
+    return mean;
+  }
+
+  // async checkUpload() {
+  //   const speed = await this.checkSpeed('upload');
+
+  //   console.log('Upload: ', speed, 'MB/s');
+  //   this.uploadSpeed = speed;
+  //   this.uploadTimeout = setTimeout(() => {
+  //     this.checkUpload();
+  //   }, MAX_TIME);
+  // }
+
+  // async checkDownload() {
+  //   const speed = await this.checkSpeed('download');
+
+  //   console.log('Download: ', speed, 'MB/s');
+  // }
+
+  // async checkSpeed(type, amount = CHECK_AMOUNT) {
+  //   const times = [];
+
+  //   for (let i = 0; i < amount; i++) {
+  //     const t0 = performance.now();
+
+  //     if (type === 'upload') {
+  //       let formData = new FormData();
+
+  //       formData.append('data', this.generateRandomData());
+  //       await API.app.checkUpload(formData);
+  //       formData = null;
+  //     } else {
+  //       await API.app.checkDownload();
+  //     }
+  //     const t1 = performance.now();
+
+  //     if (t1 - t0 > MAX_API_TIME) {
+  //       return TEST_DATA_SIZE / (t1 - t0);
+  //     }
+
+  //     times.push(Math.floor(t1 - t0));
+  //   }
+  //   const time = times.reduce((a, b) => a + b, 0) / amount;
+  //   const speed = TEST_DATA_SIZE / time;
+
+  //   return speed;
+  // }
+
+  // generateRandomData() {
+  //   return new Blob([ new ArrayBuffer(TEST_DATA_SIZE) ], { type: 'multipart/form-data' });
+  // };
 }
 
 export default new SpeedTest();
