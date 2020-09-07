1da81532ec5d5e4c2bd16f23c4d54dce3e72e4d5
Michael Nalbandyan <xinger@ya.ru>
2020-09-07 19:32:21 +0300

    Tabs component

---
+++ src/renderer/views/MainWindow/Styleguide/Tabs.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <tabs>
+      <tab
+        selected
+        name="First"
+      >
+        First tab content
+      </tab>
+      <tab name="Second">
+        Second tab content
+      </tab>
+      <tab name="Third">
+        Third tab content
+      </tab>
+    </tabs>
+  </div>
+</template>
+
+<script>
+
+import { Tabs, Tab } from '@components/Tabs';
+
+export default {
+  components: {
+    Tabs,
+    Tab,
+  },
+  data() {
+    return {
+
+    };
+  },
+
+  created() {
+
+  },
+  methods: {
+    selectItem(data) {
+      this.selectedArray = data;
+    },
+  },
+};
+</script>
+
+<style scoped lang="stylus">
+
+</style>
