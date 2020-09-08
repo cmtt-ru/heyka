043a16716d6dd4054df1dc54e6af85e33b14a6a2
ivan <bushmin@list.ru>
2020-09-08 18:19:44 +0300

    tabs, email api

--- src/api/workspace/inviteByMail.js
+++ src/api/workspace/inviteByMail.js
@@ -3,12 +3,12 @@ import axios from 'axios';
 /**
  * Send invite to a user by email
  * @param {string} id - workspace id
- * @param {string} email - email
+ * @param {string} emailList - email array
  *
  * @returns {string} result string
  */
-export default function (id, email) {
-  const res = axios.post(`/workspaces/${id}/invite/email`, { email });
+export default function (id, emailList) {
+  const res = axios.post(`/workspaces/${id}/invite/email`, { emailList });
 
   return res.data;
 }
