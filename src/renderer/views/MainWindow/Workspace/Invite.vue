46f3ba547315069756d2d1bb9f6211635db93332
ivan <bushmin@list.ru>
2020-09-08 18:19:44 +0300

    tabs, email api

--- src/renderer/views/MainWindow/Workspace/Invite.vue
+++ src/renderer/views/MainWindow/Workspace/Invite.vue
@@ -1,89 +1,107 @@
 <template>
-  <div
-    class="l-p-12"
-  >
-    <div class="tab">
-      <div class="link-wrapper">
-        <ui-button
-          v-show="!linkCopied"
-          :type="1"
-          :wide="true"
-          class="link"
-          @click="copyLinkHandler"
-        >
-          Copy invite link
-        </ui-button>
-        <ui-button
-          v-show="linkCopied"
-          :type="5"
-          :wide="true"
-          class="link"
-          @click="copyLinkHandler"
-        >
-          Copied!
-        </ui-button>
-      </div>
-      <div
-        v-show="linkCopied"
-        class="link__copied-text"
-      >
-        Your invite link expires in 1 day
-      </div>
-    </div>
+  <pseudo-popup @close="closeHandler">
+    <template #header>
+      Add to workspace
+    </template>
 
-    <div class="tab l-p-12">
-      <ui-form
-        v-if="!emailsSent"
-        @submit="sendInvites()"
-      >
-        <editable-list
-          v-model="emails"
-          add-text="Add email"
-          email
-          placeholder="name@example.com"
-          class="email-inputs"
-        />
-        <ui-button
-          :type="1"
-          submit
+    <template #body>
+      <tabs>
+        <tab
+          selected
+          name="Link"
         >
-          Send
-        </ui-button>
-      </ui-form>
-      <div v-if="emailsSent">
-        <div class="success">
-          You have successfully sent 3 invitations
-          <svg-icon
-            class="success__tick"
-            name="check"
-            stroke="var(--color-1)"
-            size="medium"
-          />
-        </div>
-        <ui-button
-          :type="1"
-          :wide="true"
-          class="link"
-          @click="resetEmails"
-        >
-          Send more invites
-        </ui-button>
-      </div>
-    </div>
-  </div>
+          <div class="link-wrapper">
+            <ui-button
+              v-show="!linkCopied"
+              :type="1"
+              :wide="true"
+              class="link"
+              @click="copyLinkHandler"
+            >
+              Copy invite link
+            </ui-button>
+            <ui-button
+              v-show="linkCopied"
+              :type="5"
+              :wide="true"
+              class="link"
+              @click="copyLinkHandler"
+            >
+              Copied!
+            </ui-button>
+          </div>
+          <div
+            v-show="linkCopied"
+            class="link__copied-text"
+          >
+            Your invite link expires in 30 days
+          </div>
+        </tab>
+
+        <tab name="Email">
+          <ui-form
+            v-if="!emailsSent"
+            @submit="sendInvites()"
+          >
+            <editable-list
+              v-model="emails"
+              add-text="Add email"
+              email
+              placeholder="name@example.com"
+              class="email-inputs"
+            />
+            <ui-button
+              :type="1"
+              submit
+            >
+              Send
+            </ui-button>
+          </ui-form>
+          <div v-if="emailsSent">
+            <div class="success">
+              You have successfully sent 3 invitations
+              <svg-icon
+                class="success__tick"
+                name="check"
+                stroke="var(--color-1)"
+                size="medium"
+              />
+            </div>
+            <ui-button
+              :type="1"
+              :wide="true"
+              class="link"
+              @click="resetEmails"
+            >
+              Send more invites
+            </ui-button>
+          </div>
+        </tab>
+
+        <tab name="Slack">
+          Work in progress
+        </tab>
+      </tabs>
+    </template>
+  </pseudo-popup>
 </template>
 
 <script>
 import UiButton from '@components/UiButton';
 import { UiForm } from '@components/Form';
+import { Tabs, Tab } from '@components/Tabs';
 import EditableList from '@components/List/EditableList';
+import PseudoPopup from '@components/PseudoPopup';
 import { mapGetters } from 'vuex';
 
 export default {
   components: {
     UiButton,
     UiForm,
+    Tabs,
+    Tab,
     EditableList,
+    PseudoPopup,
   },
 
   data() {
@@ -114,21 +132,34 @@ export default {
       try {
         const link = await this.$API.workspace.inviteByCode(this.selectedWorkspaceId);
 
-        console.log(link);
         navigator.clipboard.writeText(link.code);
         this.linkCopied = true;
       } catch (err) {
         console.log(err);
       }
     },
-    sendInvites() {
-      console.log([ ...this.emails ]);
-      this.emailsSent = true;
+    async sendInvites() {
+      try {
+        const res = await this.$API.workspace.inviteByMail(this.selectedWorkspaceId, [ ...this.emails ]);
+
+        console.log(res);
+        this.emailsSent = true;
+      } catch (err) {
+        console.log(err);
+      }
     },
 
     resetEmails() {
       this.emails = [ '' ];
       this.emailsSent = false;
+    },
+
+    /**
+     * Close handler
+     * @returns {void}
+     */
+    closeHandler() {
+      this.$router.back();
     },
   },
 
