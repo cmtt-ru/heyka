a524ea323d5fcad4ea7429c143e07f4333b1b16d
Michael Nalbandyan <xinger@ya.ru>
2020-09-28 14:19:11 +0300

    speed test

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -14,7 +14,7 @@ import '@sdk/directives';
 import { Mixin } from '@/mixins';
 import permissions from '@sdk/classes/permissions';
 import isMainWindow from '@shared/WindowManager/isMainWindow';
-import '@classes/SpeedTest';
+import '@sdk/classes/SpeedTest';
 
 if (isMainWindow()) {
   require('@classes/pushWindow');
