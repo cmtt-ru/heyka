1a38ed29d226e1b59ddcc36003c0b3e8ff0c9d25
Michael Nalbandyan <xinger@ya.ru>
2020-12-10 17:54:58 +0300

    action for conversation data & events

--- src/renderer/store/modules/channels/actions.js
+++ src/renderer/store/modules/channels/actions.js
@@ -145,4 +145,59 @@ export default {
 
     await dispatch('app/addNotification', notification, { root: true });
   },
+
+  /**
+   * Add conversation data
+   *
+   * @param {object} vuex functions
+   * @param {string} userId – user id
+   * @param {string} action – action
+   * @param {string} data – data
+   * @returns {void}
+   */
+  processConversationData({ dispatch, commit, rootGetters }, { userId, action, data }) {
+    const channelId = rootGetters['me/getSelectedChannelId'];
+
+    if (!channelId) {
+      console.error('Process Conversation Data --> no selected channel');
+
+      return;
+    }
+
+    if (!userId) {
+      console.error('Process Conversation Data --> `userId` is not defined');
+
+      return;
+    }
+
+    if (!action) {
+      console.error('Process Conversation Data --> `action` is not defined');
+
+      return;
+    }
+
+    if (!data) {
+      console.error('Process Conversation Data --> `data` is not defined');
+
+      return;
+    }
+
+    const commitData = {
+      channelId,
+      userId,
+      action,
+      data,
+    };
+
+    switch (action) {
+      case 'audio-quality-indicator':
+        commit('ADD_CONVERSATION_DATA', commitData);
+        break;
+
+      case 'hand-up':
+        commit('ADD_CONVERSATION_DATA', commitData);
+        commit('ADD_CONVERSATION_EVENT', commitData);
+        break;
+    }
+  },
 };
