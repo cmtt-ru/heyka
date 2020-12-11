1c243d5021f636b6f795bd88f227fb61751f1cac
Иван <bushmin@list.ru>
2020-12-11 13:59:03 +0300

    sharing window fix

--- src/renderer/store/modules/janus/getters.js
+++ src/renderer/store/modules/janus/getters.js
@@ -1,3 +1,5 @@
+import { IS_LINUX } from '@sdk/Constants';
+
 export default {
   /**
    * If janus in progress of an operation
@@ -8,4 +10,23 @@ export default {
   inProgress(state) {
     return state.inProgress;
   },
+
+  /**
+   * True if we are sharing whole window
+   *
+   * @param {JanusState} state – module janus state
+   * @returns {boolean}
+   */
+  isSharingFullScreen(state) {
+    const source = state.sharingSource;
+
+    const sourceId = IS_LINUX ? source?.id : source?.display_id;
+
+    if (sourceId) {
+      return true;
+    }
+
+    return false;
+  },
+
 };
