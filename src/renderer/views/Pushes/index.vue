8e5ee6f9c463144332ca30467d366731b6b32fd2
Иван <bushmin@list.ru>
2020-12-23 16:34:49 +0300

    Update index.vue

--- src/renderer/views/Pushes/index.vue
+++ src/renderer/views/Pushes/index.vue
@@ -22,10 +22,10 @@ import broadcastActions from '@sdk/classes/broadcastActions';
 import broadcastEvents from '@sdk/classes/broadcastEvents';
 import WindowManager from '@shared/WindowManager/WindowManagerRenderer';
 
-const NO_MOUSE_EVENTS_TIMEOUT = 150;
+// const NO_MOUSE_EVENTS_TIMEOUT = 3000;
 const pushWindow = WindowManager.getCurrentWindow();
 
-let ignoreMouseTimeout;
+// let ignoreMouseTimeout;
 let ignoreMouse = false;
 
 export default {
@@ -49,26 +49,31 @@ export default {
   },
 
   created() {
+    // pushWindow.api('setIgnoreMouseEvents', true, { forward: true });
     window.addEventListener('mousemove', event => {
+      // console.count(event.target);
       if (event.target === document.documentElement || event.target === document.getElementById('push-wrapper')) {
         if (ignoreMouse === true) {
           return;
         }
         pushWindow.api('setIgnoreMouseEvents', true, { forward: true });
         ignoreMouse = true;
-        if (ignoreMouseTimeout) {
-          clearTimeout(ignoreMouseTimeout);
-        }
-        ignoreMouseTimeout = setTimeout(function () {
-          pushWindow.api('setIgnoreMouseEvents', false);
-          ignoreMouse = false;
-        }, NO_MOUSE_EVENTS_TIMEOUT);
+        console.log('ignoreMouse: true');
+        // if (ignoreMouseTimeout) {
+        //   clearTimeout(ignoreMouseTimeout);
+        // }
+        // ignoreMouseTimeout = setTimeout(function () {
+        //   pushWindow.api('setIgnoreMouseEvents', false);
+        //   ignoreMouse = false;
+        //   console.log('ignoreMouse: false');
+        // }, NO_MOUSE_EVENTS_TIMEOUT);
       } else {
         if (ignoreMouse === false) {
           return;
         }
         pushWindow.api('setIgnoreMouseEvents', false);
         ignoreMouse = false;
+        console.log('ignoreMouse: false');
       }
     });
   },
