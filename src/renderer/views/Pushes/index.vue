698b319d62303d3a069d88df7ca5ea037a7c97c0
Иван <bushmin@list.ru>
2020-12-25 19:15:30 +0300

    Update index.vue

--- src/renderer/views/Pushes/index.vue
+++ src/renderer/views/Pushes/index.vue
@@ -56,7 +56,7 @@ export default {
             return;
           }
 
-          pushWindow.api('setIgnoreMouseEvents', true, { forward: true });
+          pushWindow.api('setIgnoreMouseEvents', true);
           ignoreMouse = true;
 
           if (ignoreMouseTimeout) {
