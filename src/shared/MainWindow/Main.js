b5b4a21bfcdfcb24ae15d20621bc842762f47a14
Michael Nalbandyan <xinger@ya.ru>
2021-02-10 16:39:37 +0300

    Update Main.js

--- src/shared/MainWindow/Main.js
+++ src/shared/MainWindow/Main.js
@@ -6,7 +6,6 @@ import Autoupdater from '../../main/classes/AutoUpdater';
 import { ipcMain, nativeTheme, powerMonitor, app, globalShortcut } from 'electron';
 import { heykaStore } from '../../renderer/store/localStore';
 import { IS_DEV, IS_MAC } from '../../sdk/Constants';
-import sleep from 'es7-sleep';
 
 const resizeable = heykaStore.get('resizeWindow', false);
 
@@ -143,14 +142,6 @@ class MainWindow {
 
     ipcMain.on('start-is-ready', () => {
       DeepLink.resendLast();
-    });
-
-    ipcMain.handle('test-sync', async (event, timeout = 0) => {
-      await sleep(timeout);
-
-      console.log('test-sync');
-
-      return 'test';
     });
 
     /**
