47656f8bd9bd944c359a9e959a226d1751de5880
Michael Nalbandyan <xinger@ya.ru>
2021-04-16 14:45:54 +0300

    autoupdater logs

--- src/main/classes/LogManager.js
+++ src/main/classes/LogManager.js
@@ -4,11 +4,15 @@ import { ipcMain } from 'electron';
 
 let LOG_PATH = '';
 
+export const autoupdaterLogger = log.create('autoupdater');
+
 log.transports.file.resolvePath = (variables) => {
   LOG_PATH = variables.libraryDefaultDir;
 
   return path.join(LOG_PATH, 'main.log');
 };
+
+autoupdaterLogger.transports.file.resolvePath = () => path.join(LOG_PATH, 'autoupdater.log');
 
 ipcMain.on('log-manager-path', (event, name) => {
   event.returnValue = path.join(LOG_PATH, `${name}.log`);
