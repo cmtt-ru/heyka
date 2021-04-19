09899c79f409f5b0597613a58b81153e8f2d77ae
Michael Nalbandyan <xinger@ya.ru>
2021-04-19 20:50:25 +0300

    make zip & upload logs to server

--- src/main/classes/LogManager.js
+++ src/main/classes/LogManager.js
@@ -2,29 +2,141 @@ import path from 'path';
 import log from 'electron-log';
 import { ipcMain, app } from 'electron';
 import os from 'os';
+import osName from 'os-name';
+import AdmZip from 'adm-zip';
+import FormData from 'form-data';
+import axios from 'axios';
+import fs from 'fs';
+import { API_URL } from '../Constants';
+import { authFileStore } from '../localStore';
 
-let LOG_PATH = '';
+const asyncFs = fs.promises;
 
 export const autoupdaterLogger = log.create('autoupdater');
 
+const MAIN_LOG_NAME = 'main.log';
+const AUTO_UPDATER_LOG_NAME = 'autoupdater.log';
+const ARCHIVE_NAME = 'archive.zip';
+
+let LOG_PATH = '';
+let ARCHIVE_PATH = '';
+
+/**
+ * Setting log path & name
+ * @param {object} variables – useful system paths
+ * @returns {string}
+ */
 log.transports.file.resolvePath = (variables) => {
   LOG_PATH = variables.libraryDefaultDir;
 
-  return path.join(LOG_PATH, 'main.log');
+  return path.join(LOG_PATH, MAIN_LOG_NAME);
 };
 
-autoupdaterLogger.transports.file.resolvePath = () => path.join(LOG_PATH, 'autoupdater.log');
+/**
+ * Setting auto updater log path & name
+ * @returns {string}
+ */
+autoupdaterLogger.transports.file.resolvePath = () => {
+  return path.join(LOG_PATH, AUTO_UPDATER_LOG_NAME);
+};
 
+/**
+ * Include uncaught & promise errors in logs
+ */
+log.catchErrors();
+
+/**
+ * Extend console with electron-log methods
+ */
+Object.assign(console, log.functions);
+
+/**
+ * Returns log path to renderer process
+ */
 ipcMain.on('log-manager-path', (event, name) => {
   event.returnValue = path.join(LOG_PATH, `${name}.log`);
 });
 
-log.catchErrors();
-
-Object.assign(console, log.functions);
+/**
+ * Event for sending logs to server
+ */
+ipcMain.on('log-manager-send', (event, name) => {
+  sendLogs();
+});
 
 /**
- * Sho app launched log and some useful information about computer
+ * Send logs to server
+ * @returns {Promise<void>}
+ */
+async function sendLogs() {
+  await makeZip();
+
+  const form = new FormData();
+  const accessToken = await authFileStore.get('accessToken');
+
+  form.append('logs', fs.createReadStream(ARCHIVE_PATH));
+
+  const request = {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      ...form.getHeaders(),
+    },
+  };
+
+  try {
+    await axios.post(`${API_URL}/logs`, form, request);
+
+    await clearLogs();
+  } catch (e) {
+    console.error('Error in uploading logs', e);
+  }
+}
+
+/**
+ * Make zip archive with all logs
+ * @returns {void}
+ */
+async function makeZip() {
+  const zip = new AdmZip();
+  const files = await getLogFileNames();
+
+  /** Adding files to archive */
+  files.forEach(file => zip.addLocalFile(path.join(LOG_PATH, file)));
+
+  ARCHIVE_PATH = path.join(LOG_PATH, ARCHIVE_NAME);
+
+  /** Write archive to file */
+  zip.writeZip(ARCHIVE_PATH);
+}
+
+/**
+ * Get log file names
+ * @returns {Promise<array>}
+ */
+async function getLogFileNames() {
+  const extensions = [ '.log' ];
+
+  try {
+    const files = await asyncFs.readdir(LOG_PATH);
+
+    return files.filter(f => extensions.includes(path.extname(f).toLowerCase()));
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+/**
+ * Clear log files
+ * @returns {Promise<void>}
+ */
+async function clearLogs() {
+  const files = await getLogFileNames();
+
+  files.forEach(file => asyncFs.unlink(path.join(LOG_PATH, file)));
+}
+
+/**
+ * Show app launched log and some useful information about computer
  */
 const mb = 1048576;
 const cpus = os.cpus();
@@ -38,7 +150,7 @@ console.log(`\n\n
 ╚═╝  ╚═╝╚═╝     ╚═╝         ╚══════╝╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═════╝
 
 App version: ${app.getVersion()}
-OS: ${os.type()}
+OS: ${osName()} ${os.release()}
 CPU: ${cpus[0].model} / ${cpus.length} cores
 RAM: ${Math.round(os.totalmem() / mb)}MB
 `);
