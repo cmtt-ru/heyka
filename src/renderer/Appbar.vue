e47336d6ea47cd6ec008c5c253340cea9b42317a
Michael Nalbandyan <xinger@ya.ru>
2021-04-21 19:48:40 +0300

    TMP FIX

--- src/renderer/Appbar.vue
+++ src/renderer/Appbar.vue
@@ -231,10 +231,9 @@ export default {
     },
 
     toggleSettings() {
-      console.log(this.$route);
-      if (this.$route.fullPath.includes('/settings')) {
+      if (this.$route.name === 'settings') {
         this.__backOrRedirect();
-      } else if (this.$route.fullPath.includes('/styleguide')) {
+      } else if (this.$route.name === 'styleguide') {
         this.$router.replace({ name: 'workspace' });
       } else {
         this.$router.push({ name: 'settings' });
