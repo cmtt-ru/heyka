74a84f55287e08d4d111cc737cc24cf932c1baaa
Michael Nalbandyan <xinger@ya.ru>
2021-07-28 17:32:33 +0300

    log secrets

--- notarize.js
+++ notarize.js
@@ -5,15 +5,18 @@ module.exports = async (context) => {
 
   if (electronPlatformName === 'darwin') {
     try {
-      console.log('Try notarize app');
       const appName = context.packager.appInfo.productFilename;
 
-      await notarize({
+      const data = {
         appBundleId: 'app.live.hejka',
         appPath: `${appOutDir}/${appName}.app`,
         appleId: process.env.APPLE_DEVELOPER_ID,
         appleIdPassword: process.env.APPLE_ASP,
-      });
+      };
+
+      console.log('Try notarize app', data);
+
+      await notarize(data);
       console.log('Success notarize');
     } catch (err) {
       console.log(err);
